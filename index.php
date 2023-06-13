67ec45b18d97595eb36c1544b6f6be2a09a5834c
shiroichi28 <manigandans28@gmail.com>
2023-06-13 21:20:03 +0530

    Added action buttons

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+include('./includes/db.php');
 session_start();
 ?>
 <!DOCTYPE html>
@@ -16,16 +18,32 @@ session_start();
     <div class="container">
         <div class="row mt-5">
             <a href="ae.php"><button class="btn-primary btn ">Add</button></a>
-                <table class="table table-bordered mt-3">
-                    <thead>
-                        <th>S.no</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                        <th>Actions</th>
-                    </thead>
-                </table>
-            </div>
+            <table class="table table-bordered mt-3">
+                <thead>
+                    <th>S.no</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Mobile</th>
+                    <th colspan="2">Actions</th>
+                </thead>
+                <tbody>
+                    <?php $stmt = $pdo->query("SELECT * FROM profile ");
+                    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+                    <?= empty($rows) ? '<tr ><td colspan="6">No data</td></tr>' : "" ?>
+                    <?php foreach ($rows as $i => $row) : ?>
+                        <tr>
+                            <td><?= $i + 1 ?></td>
+                            <td><?= $row['username'] ?></td>
+                            <td><?= $row['email'] ?></td>
+                            <td><?= $row['mobile'] ?></td>
+                            <td><a href="ae.php?id=<?= $row['id'] ?>"><button class="btn btn-primary">Edit</button></a></td>
+                            <td><a href="delete.php?id=<?= $row['id'] ?>"><button class="btn btn-danger">Delete</button></a></td>
+                        </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </div>
 
 
     </div>
