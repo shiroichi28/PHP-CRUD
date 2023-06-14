c1200dd117768fa8c45a39089f816ab8e2fe2a9b
shiroichi28 <manigandans28@gmail.com>
2023-06-14 22:19:32 +0530

    update

--- index.php
+++ index.php
@@ -50,6 +50,7 @@ session_start();
 
 
     </div>
+    <!-- Script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
